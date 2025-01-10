6bb1f02b7cc98a1bae2f04b747c879d11fe36fe0
xTEJ30 <56229589+xTEJ30@users.noreply.github.com>
2025-01-09 19:10:38 -0500

    maze game

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,132 @@
-package PACKAGE_NAME;public class Hero {
+public class Hero
+{
+    int r,c,steps;
+    String [][]maze;
+    boolean [][] visitedLocs;
+    String direction = "S";
+    public Hero(int r, int c,String direction,String[][] maze)
+    {
+        this.r=r;
+        this.c=c;
+        this.maze=maze;
+        this.steps = steps;
+        this.direction = direction;
+    }
+    public void move(int key)
+    {
+
+        if(key==39)//turn right
+        {
+            if(direction.equals("N"))
+            {
+                direction= "E";
+
+            }
+            else if(direction.equals("E"))
+            {
+                direction= "S";
+            }
+            else if(direction.equals("S"))
+            {
+                direction= "W";
+            }
+            else if(direction.equals("W"))
+            {
+                direction= "N";
+            }
+
+        }
+        if(key==37)//turn left
+        {
+            if(direction.equals("N"))
+            {
+                direction= "W";
+            }
+            else if(direction.equals("W"))
+            {
+                direction= "S";
+            }
+            else if(direction.equals("S"))
+            {
+                direction= "E";
+            }
+            else if(direction.equals("E"))
+            {
+                direction= "N";
+            }
+
+        }
+        if(key==38)//forward
+        {
+            boolean moved = false;
+            if(direction.equals("N"))//move North
+            {
+                try
+                {
+                    if(maze[r-1][c].equals(" "))
+                    {
+                        r--;
+                        moved = true;
+
+                    }
+                }catch(ArrayIndexOutOfBoundsException e){}
+            }
+            if(direction.equals("W"))//move West
+            {
+                try{
+                    if(maze[r][c-1].equals(" "))
+                    {
+                        c--;
+                        moved = true;
+
+                    }
+                }catch(ArrayIndexOutOfBoundsException e){}
+            }
+            if(direction.equals("E"))//move East
+            {
+                try{
+                    if(maze[r][c+1].equals(" "))
+                    {
+                        c++;
+                        moved = true;
+
+                    }
+                }catch(ArrayIndexOutOfBoundsException e){}
+
+            }
+            if(direction.equals("S"))//move East
+            {
+                try{
+                    if(maze[r+1][c].equals(" "))
+                    {
+                        r++;
+                        moved = true;
+
+                    }
+
+                }catch(ArrayIndexOutOfBoundsException e){}
+            }
+            if(moved) { steps++; }
+        }
+
+
+    }
+    public int getRow()
+    {
+        return r;
+    }
+
+    public int getCol()
+    {
+        return c;
+    }
+    public String getDirection()
+    {
+        return direction;
+    }
+
+    public int getsteps(){
+        return steps;
+    }
+
 }
--- src/Wall.java
+++ src/Wall.java
@@ -1,2 +1,97 @@
-package PACKAGE_NAME;public class Wall {
+import java.awt.Polygon;
+import java.awt.GradientPaint;
+import java.awt.Color;
+public class Wall{
+    private int[] x;
+    private int[] y;
+    private char dir;
+    private int k;
+    private String type;
+    public Wall(int[] x, int[] y, char dir,int k, String type){ // instance var to designate individual gradient
+        this.y = y;
+        this.x = x;
+        this.dir = dir;
+        this.k = k;
+        this.type = type;
+    }
+
+    public Polygon getPoly(){
+        return new Polygon(x,y,y.length);
+    }
+
+    public int[] getStartCoord(){
+        int[] result = new int[2];
+        switch(dir)
+        {
+            case 'U':
+                result[0] = x[3];
+                result[1] = y[0];
+                break;
+
+            case 'D':
+                result[0] = x[3];
+                result[1] = y[0];
+
+            case 'L':
+                result[0] = x[0];
+                result[1] = y[3];
+
+            case 'R':
+                result[0] = x[0];
+                result[1] = y[3];
+
+            case 'F':
+                result[0] = x[0];
+                result[1] = y[0];
+                break;
+        }
+        return result;
+    }
+
+    public int[] getEndCoord(){
+        return new int[]{x[3], y[3]};
+    }
+
+    public int[] getX(){
+        return this.x;
+    }
+
+    public int[] getY(){
+        return this.y;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public GradientPaint getPaint()
+    {
+        int eR = k-50;
+        int eG = k-50;
+        int eB = k-50;
+
+        if(k<0)
+            k=0;
+        if(eR<0)
+            eR = 0;
+        if(eG<0)
+            eG = 0;
+        if(eB<0)
+            eB = 0;
+
+        switch(type)
+        {
+            case "left":
+            case "right":
+                return new GradientPaint(x[0], y[1], new Color(k, k, k), x[1], y[1], new Color(eR, eG, eB));
+
+        }
+        return new GradientPaint(x[0], y[3], new Color(k, k, k), x[0], y[0], new Color(eR, eG, eB));
+
+    }
+
+
+
+    // make an instance of the start and end coordinates
+
 }
